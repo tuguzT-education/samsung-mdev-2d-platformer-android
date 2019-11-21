31a5e0a9df15429155833bd543cb243388d06981
tuguzT <56771526+tuguzT@users.noreply.github.com>
2019-11-21 20:06:28 +0300

    The very beginning

---
+++ app/src/main/java/com/example/a2d_platformer/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.a2d_platformer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+}
