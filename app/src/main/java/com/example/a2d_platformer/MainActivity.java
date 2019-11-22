4b8b2d14cb66a1d878d171cf6656ab446d93750d
tuguzT <56771526+tuguzT@users.noreply.github.com>
2019-11-22 19:18:09 +0300

    Navigation bar was hidden natively

--- app/src/main/java/com/example/a2d_platformer/MainActivity.java
+++ app/src/main/java/com/example/a2d_platformer/MainActivity.java
@@ -4,15 +4,41 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    /* Этот метод при вызове убирает надоедливую навигационную панель */
+    void setImmersiveSticky() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setImmersiveSticky();
+        getWindow().getDecorView().setOnSystemUiVisibilityChangeListener((new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                setImmersiveSticky();
+            }
+        }));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setImmersiveSticky();
     }
 
     @Override
